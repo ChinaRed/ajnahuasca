8946a4ee1bae1008b843863f1421f3fdb37f1800
Jon Tan <joanthan.tan83@gmail.com>
2017-07-24 21:31:50 -0700

    adds logo image, styles header, nav, logo

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -1,18 +1,17 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  // HOME page click tile links
-  // 
-  // // Open in new window
-  // $(".box1").click(function () {
-  //     window.open($(this).find("a:first").attr("href"));
-  //     return false;
-  // });
+//   // HOME page click tile links
+//   // 
+//   // // Open in new window
+//   // $(".box1").click(function () {
+//   //     window.open($(this).find("a:first").attr("href"));
+//   //     return false;
+//   // });
 
-  // Or use this to Open link in same window (similar to target=_blank)
-  $(".table-center").click(function(){
-      window.location = $(this).find("a:first").attr("href");
-      return false;
-  });
+//   // Or use this to Open link in same window (similar to target=_blank)
+//   $(".table-center").click(function(){
+//       window.location = $(this).find("a:first").attr("href");
+//       return false;
+//   });
+// });
 
-  
-});
