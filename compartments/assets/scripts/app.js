68456ecb1442ec0393adb5d8b4eb3f9bc3f69dde
Jon Tan <joanthan.tan83@gmail.com>
2017-07-24 22:40:26 -0700

    adds condensed nav on scroll, updates logo

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -1,17 +1,43 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//   // HOME page click tile links
-//   // 
-//   // // Open in new window
-//   // $(".box1").click(function () {
-//   //     window.open($(this).find("a:first").attr("href"));
-//   //     return false;
-//   // });
+  // HOME page click tile links
+  // 
+  // // Open in new window
+  // $(".box1").click(function () {
+  //     window.open($(this).find("a:first").attr("href"));
+  //     return false;
+  // });
 
-//   // Or use this to Open link in same window (similar to target=_blank)
-//   $(".table-center").click(function(){
-//       window.location = $(this).find("a:first").attr("href");
-//       return false;
-//   });
-// });
+  // brings user back to home
+  $(".logo_container").click(function(){
+      window.location = $(this).find("a:first").attr("href");
+      return false;
+  });
+
+  window.addEventListener('scroll', function(e){
+      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+          shrinkOn = 80,
+          header = document.querySelector("header");
+      if (distanceY > shrinkOn) {
+          $("header").addClass("condensed");
+      } else {
+          if ($("header").has("condensed")) {
+              $("header").removeClass("condensed");
+          }
+      }
+  });
+
+  // $(document).scroll(function() {
+  //   if ($(this).scrollTop() > 0) {
+  //     $('.floating_nav_content').addClass('.extended_header').removeClass('.floating_nav_content');
+  //     console.log('scroll');
+  //   }
+  //   if ($(this).scrollTop == 0) {
+  //     $('.floating_nav_content').removeClass('.extended_header');
+  //   }
+  //   else{
+  //     return;
+  //   }
+  // });
+});
 
