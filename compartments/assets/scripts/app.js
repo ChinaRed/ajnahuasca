5d789a33d88a313929101d60ffe4988094f3bd1b
Jon Tan <joanthan.tan83@gmail.com>
2017-07-30 19:54:05 -0700

    adds responsive nav for mobile

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -1,43 +1,22 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  // HOME page click tile links
-  // 
-  // // Open in new window
-  // $(".box1").click(function () {
-  //     window.open($(this).find("a:first").attr("href"));
-  //     return false;
-  // });
+//   // Logo link to home page
+//   $(".logo_container").click(function(){
+//       window.location = $(this).find("a:first").attr("href");
+//       return false;
+//   });
 
-  // brings user back to home
-  $(".logo_container").click(function(){
-      window.location = $(this).find("a:first").attr("href");
-      return false;
-  });
-
-  window.addEventListener('scroll', function(e){
-      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-          shrinkOn = 80,
-          header = document.querySelector("header");
-      if (distanceY > shrinkOn) {
-          $("header").addClass("condensed");
-      } else {
-          if ($("header").has("condensed")) {
-              $("header").removeClass("condensed");
-          }
-      }
-  });
-
-  // $(document).scroll(function() {
-  //   if ($(this).scrollTop() > 0) {
-  //     $('.floating_nav_content').addClass('.extended_header').removeClass('.floating_nav_content');
-  //     console.log('scroll');
-  //   }
-  //   if ($(this).scrollTop == 0) {
-  //     $('.floating_nav_content').removeClass('.extended_header');
-  //   }
-  //   else{
-  //     return;
-  //   }
-  // });
-});
+//   window.addEventListener('scroll', function(e){
+//       var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+//           shrinkOn = 80,
+//           header = document.querySelector("header");
+//       if (distanceY > shrinkOn) {
+//           $("header").addClass("condensed");
+//       } else {
+//           if ($("header").has("condensed")) {
+//               $("header").removeClass("condensed");
+//           }
+//       }
+//   });
+// });
 
