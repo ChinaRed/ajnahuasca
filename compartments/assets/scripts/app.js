8b32a7cf573b6d760bd75478d1765b09e45181e2
Jon Tan <joanthan.tan83@gmail.com>
2017-08-04 01:19:55 -0700

    removes jQuery scroll triggered transition, adds font size ratios to P, H1, H2

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
   // Logo link to home page
   // $(".logo_container").click(function(){
@@ -6,21 +6,21 @@ $(document).ready(function () {
   //     return false;
   // });
 
-  window.addEventListener('scroll', function(e){
-      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-          shrinkOn = 80,
-          header = document.querySelector("header");
-          menu = document.querySelector(".menu-item");
-      if (distanceY > shrinkOn) {
-          $("header").addClass("condensed");
-          $(".menu-item").addClass("condensed");
-          console.log('menu');
-      } else {
-          if ($("header").has("condensed") && $(".menu-item").has("condensed")) {
-              $("header").removeClass("condensed");
-              $(".menu-item").removeClass("condensed");
-          }
-      }
-  });
-});
+//   window.addEventListener('scroll', function(e){
+//       var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+//           shrinkOn = 80,
+//           header = document.querySelector("header");
+//           menu = document.querySelector(".menu-item");
+//       if (distanceY > shrinkOn) {
+//           $("header").addClass("condensed");
+//           $(".menu-item").addClass("condensed");
+//           console.log('menu');
+//       } else {
+//           if ($("header").has("condensed") && $(".menu-item").has("condensed")) {
+//               $("header").removeClass("condensed");
+//               $(".menu-item").removeClass("condensed");
+//           }
+//       }
+//   });
+// });
 
