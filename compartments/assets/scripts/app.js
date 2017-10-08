ab22abcc7aea52e50f150a6248f415eb71ca27fb
Jon Tan <joanthan.tan83@gmail.com>
2017-10-07 17:31:50 -0700

    styles blog listing to add underline and text color change on hover, reduces width, puts title on top of date, adjusts padding of blog info div

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -14,35 +14,35 @@ $(document).ready(function(){
    }, 100);
   }
 
-$(function() {
+// $(function() {
 
-    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
-    $fluidEl = $("figure");
+//     var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+//     $fluidEl = $("figure");
 
-  $allVideos.each(function() {
+//   $allVideos.each(function() {
 
-    $(this)
-      // jQuery .data does not work on object/embed elements
-      .attr('data-aspectRatio', this.height / this.width)
-      .removeAttr('height')
-      .removeAttr('width');
+//     $(this)
+//       // jQuery .data does not work on object/embed elements
+//       .attr('data-aspectRatio', this.height / this.width)
+//       .removeAttr('height')
+//       .removeAttr('width');
 
-  });
+//   });
 
-  $(window).resize(function() {
+//   $(window).resize(function() {
 
-    var newWidth = $fluidEl.width();
-    $allVideos.each(function() {
+//     var newWidth = $fluidEl.width();
+//     $allVideos.each(function() {
 
-      var $el = $(this);
-      $el
-          .width(newWidth)
-          .height(newWidth * $el.attr('data-aspectRatio'));
+//       var $el = $(this);
+//       $el
+//           .width(newWidth)
+//           .height(newWidth * $el.attr('data-aspectRatio'));
 
-    });
+//     });
 
-  }).resize();
-});
+//   }).resize();
+// });
 
 //   $("#message_link").click(function(){
 //    setTimeout(function() {
